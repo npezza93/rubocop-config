aceef71d92e5d797826d237c8cefc7b1bb5d0371
Christopher Pezza <pezza.chris@gmail.com>
2017-01-24 21:44:03 -0500

    Add Dynamic Ruby version definition in default.yml

--- lib/rubocop_config/runner.rb
+++ lib/rubocop_config/runner.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+require "erb"
+require "yaml"
+
 module RubocopConfig
   class Runner
     attr_accessor :pwd
@@ -10,14 +13,14 @@ module RubocopConfig
     def perform
       file_name = "#{pwd}/.rubocop.yml"
 
-      File.open(file_name, "w") { |f| f << config }
+      File.open(file_name, "w") { |f| f << config.to_yaml }
     end
 
     def config
       # binding.pry
-      @config ||= File.read(
+      @config ||= YAML.safe_load(ERB.new(File.read(
         File.expand_path("../../../config", __FILE__) + "/default.yml"
-      )
+      )).result)
     end
   end
 end
